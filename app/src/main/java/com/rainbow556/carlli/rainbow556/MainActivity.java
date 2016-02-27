7e114097483352e9632d48ceab8ebafa6c3740a1
Rainbow556 <903263494@qq.com>
2016-02-27 14:37:51 +0800

    22

---
+++ app/src/main/java/com/rainbow556/carlli/rainbow556/MainActivity.java
@@ -0,0 +1,67 @@
+package com.rainbow556.carlli.rainbow556;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity{
+    private String mStr;
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+            }
+        });
+        btn = new Button(this);
+        View.OnClickListener mOnClickListener = null;
+        int i = 5;
+        if(i == 5){
+            Log.e("lx", "kx    ");
+        }
+        i += 5;
+        ArrayList<String> list = new ArrayList<String>();
+        for(String s : list){
+        }
+        btn.setTextColor(0);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    /**
+     * @param s
+     */
+    private void initMethod(String s){
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.action_settings){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
